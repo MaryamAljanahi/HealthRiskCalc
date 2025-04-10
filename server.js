4ef4792069bcf281b9df94060232b233af19fe78
MaryamAljanahi <g00091582@aus.edu>
2025-04-10 09:21:22 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express(); // Initialize Express app
-const port = 3000;
+const port = 8080;
 
 // Middleware
 app.use(cors());
